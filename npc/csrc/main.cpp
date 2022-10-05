feef2ec6d44d772eadc83768bef62d6b21c812f7
pluveto <i@pluvet.com>
2022-10-05 21:30:13 +0800

    feat: switch

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,15 +1,24 @@
 #include <verilated.h>
+#include <verilated_vcd_c.h>
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
+
 #include "Vtop.h"
 
 int main(int argc, char const* argv[]) {
-  static const int N = 10;
   VerilatedContext contextp;
   contextp.commandArgs(argc, argv);
+  Verilated::traceEverOn(true);
+  VerilatedVcdC tfp;
+
   Vtop top(&contextp);
-  for (int i = 0; i < N; i++) {
+  top.trace(&tfp, 99);
+  tfp.open("obj_dir/simx.vcd");
+  int sim_time = 100;
+  for (; contextp.time() < sim_time && !contextp.gotFinish();) {
+    contextp.timeInc(1);
+
     int a = rand() & 1;
     int b = rand() & 1;
     top.a = a;
@@ -17,7 +26,9 @@ int main(int argc, char const* argv[]) {
     top.eval();
     printf("a = %d, b = %d, f = %d\n", a, b, top.f);
     assert(top.f == (a ^ b));
+    tfp.dump(contextp.time());
   }
+  tfp.close();
 
   return 0;
 }
