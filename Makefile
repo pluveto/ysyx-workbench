14ea3ca0f5217dc65a8d185bf5acb077bdb43c1e
pluveto <i@pluvet.com>
2022-10-05 20:14:08 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard f42b05e1f03e369cfc6701efc8737cda972d2a24 keyboard: fix spelling mistake, fix #9

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_22050523
+STUNAME = 张梓靖
 
 # DO NOT modify the following code!!!
 
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include <verilated.h>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include "Vtop.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char const* argv[]) {
+  static const int N = 10;
+  VerilatedContext contextp;
+  contextp.commandArgs(argc, argv);
+  Vtop top(&contextp);
+  for (int i = 0; i < N; i++) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top.a = a;
+    top.b = b;
+    top.eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top.f);
+    assert(top.f == (a ^ b));
+  }
+
   return 0;
 }
